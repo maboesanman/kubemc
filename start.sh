f761863d149c46671083e8c4e644c5c1ea12f985
Mason Boeman <masonboeman@gmail.com>
2021-07-30 20:59:09 -0500

    get lobby working

--- start.sh
+++ start.sh
@@ -8,14 +8,10 @@ docker build -t mc-velocity mc-velocity
 
 # load docker images into container
 kind load docker-image --name kubemc mc-limbo
-# kind load docker-image --name kubemc mc-velocity
+kind load docker-image --name kubemc mc-velocity
 
 # apply configs
 kubectl apply -f k8s-limbo-deployment.yml
 kubectl apply -f k8s-limbo-service.yml
-# kubectl apply -f k8s-velocity-deployment.yml
-# kubectl apply -f k8s-velocity-service.yml
-
-# port forward
-# kubectl wait --for=condition=ready service/velocity-service
-# kubectl port-forward service/velocity-service --address 0.0.0.0 25565:25565
+kubectl apply -f k8s-velocity-deployment.yml
+kubectl apply -f k8s-velocity-service.yml
