cdb983680ac89a87cd3b6e6bb0efbe587610f308
Mason Boeman <masonboeman@gmail.com>
2021-07-30 20:03:47 -0500

    get nodeport working with just limbo server

--- start.sh
+++ start.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+# create cluster
 kind create cluster --config kind-config.yml
 
-kind load docker-image --name kubemc mc-limbo
+docker build -t mc-limbo mc-limbo
+docker build -t mc-velocity mc-velocity
 
+# load docker images into container
+kind load docker-image --name kubemc mc-limbo
+# kind load docker-image --name kubemc mc-velocity
+
+# apply configs
 kubectl apply -f k8s-limbo-deployment.yml
 kubectl apply -f k8s-limbo-service.yml
-kubectl apply -f k8s-ingress.yml
+# kubectl apply -f k8s-velocity-deployment.yml
+# kubectl apply -f k8s-velocity-service.yml
+
+# port forward
+# kubectl wait --for=condition=ready service/velocity-service
+# kubectl port-forward service/velocity-service --address 0.0.0.0 25565:25565
