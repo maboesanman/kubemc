916108dd74ce3a88c3c5346a8cb367ea7a544c2a
Mason Boeman <masonboeman@gmail.com>
2022-01-01 21:23:55 -0600

    switch to aws docker images

--- down.sh
+++ down.sh
@@ -1,17 +1,13 @@
-# this shouldn't be here but just in case
-kubectl delete pod seeder
-
-# delete everything that isn't world data
 kubectl delete deployment velocity-deployment
 kubectl delete service velocity-service
-kubectl delete pvc velocity-config
 
 kubectl delete deployment survival-deployment
 kubectl delete service survival-service
-kubectl delete pvc survival-config
-kubectl delete pvc survival-mods
+
+kubectl delete deployment survival-laurel-deployment
+kubectl delete service survival-laurel-service
 
 kubectl delete deployment creative-deployment
 kubectl delete service creative-service
-kubectl delete pvc creative-config
-kubectl delete pvc creative-mods
+
+kubectl delete secret player-forwarding-secret
--- up.sh
+++ up.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# seed configs and mods
-kubectl apply -f k8s-seed.yml
-kubectl apply -f k8s-storage.yml
-kubectl wait --for=condition=ready pod seeder
-find seed \( ! -path '*/.*' \) -type f -exec kubectl cp ./{} seeder:/{} \;
-kubectl delete pod seeder --wait
+# create secret
+kubectl create secret generic player-forwarding-secret --from-literal=secret=$(openssl rand -hex 16)
 
 # spin up services
 kubectl apply -f k8s-velocity.yml
-kubectl apply -f k8s-survival.yml
-kubectl apply -f k8s-creative.yml
+kubectl apply -f k8s-server-survival.yml
+kubectl apply -f k8s-server-survival-laurel.yml
+kubectl apply -f k8s-server-creative.yml
